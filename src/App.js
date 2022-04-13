60afc3e8b189fdcd9032e5fe65d5db280600793b
TANNER MECK <steve.meck246@gmail.com>
2022-04-13 11:57:39 -0700

    question one rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { data } from './services/data.js';
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    const fetchData = () => {
+      setQuestions(data);
+    };
+    fetchData();
+  }, []);
+
+  const handleStartGame = () => {
+    setStart(true);
+  };
+
+  console.log(questions[0]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!start && <button onClick={handleStartGame}>Start the Game</button>}
+      {start && (
+        <div>
+          <h1>{questions[index].question}</h1>
+          {questions[index].options.map((option, index) => (
+            <section key={index}>
+              <label>{option}</label>
+              <input type="radio" name="question" value={option} />
+            </section>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/services/data.js
@@ -0,0 +1,18 @@
+const data = [
+  {
+    question: 'How many penguins do bears eat?',
+    options: ['none', 'one', 'two', 'all'],
+    solutionIndex: 0,
+  },
+  {
+    question: 'What is one plus one?',
+    options: ['none', 'one', 'two', 'all'],
+    solutionIndex: 2,
+  },
+  {
+    question: 'How many bread sticks can you eat?',
+    options: ['none', 'one', 'two', 'olive em'],
+    solutionIndex: 3,
+  },
+];
+export { data };
