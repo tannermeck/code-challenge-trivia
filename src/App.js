49da929b3af983ba39367d55c5c87945656dccd5
TANNER MECK <steve.meck246@gmail.com>
2022-04-14 11:26:13 -0700

    timer working

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ function App() {
   const [answer, setAnswer] = useState(null);
   const [correct, setCorrect] = useState(false);
   const [incorrect, setIncorrect] = useState(false);
-  const [timer, setTimer] = useState(30);
+  const [timer, setTimer] = useState(5);
   const [answerIndex, setAnswerIndex] = useState(0);
   const [disabled, setDisabled] = useState(false);
   const [score, setScore] = useState(0);
+  const [hideTimer, setHideTimer] = useState(false);
 
   useEffect(() => {
     const fetchData = () => {
@@ -20,22 +21,28 @@ function App() {
     };
     fetchData();
   }, []);
-  // const countDown = () => {
-  //   console.log(timer);
-  //   while (timer > 0) {
-  //     const timer = setTimeout(() => {
-  //       setTimer(timer - 1);
-  //     }, 1000);
-  //   }
-  //   return () => {
-  //     clearTimeout(timer);
-  //   };
-  // };
+
+  useEffect(() => {
+    if (start && !hideTimer) {
+      timer > 0 &&
+        setTimeout(() => {
+          setTimer(timer - 1);
+        }, 1000);
+    }
+    if (!hideTimer && timer === 0) {
+      setDisabled(true);
+      setAnswer(null);
+      setIncorrect(true);
+    }
+  }, [hideTimer, start, timer]);
+
   const handleStartGame = () => {
     setStart(true);
   };
   const handleSubmitAnswer = () => {
     setDisabled(true);
+    //hide timer when answer is submitted
+    setHideTimer(true);
     if (+questions[index].solutionIndex === +answer) {
       setCorrect(true);
       setScore(score + 1);
@@ -56,6 +63,8 @@ function App() {
   };
   const handleNext = async () => {
     setDisabled(false);
+    setTimer(5);
+    setHideTimer(false);
     if (index <= 3) {
       setIndex(index + 1);
       setAnswerIndex(questions[index + 1].solutionIndex);
@@ -70,15 +79,15 @@ function App() {
     setCorrect(null);
     setIncorrect(null);
     setDisabled(false);
-    console.log('index', index);
-    console.log('solution', questions[index].solutionIndex);
+    setHideTimer(false);
+    setTimer(5);
   };
   return (
     <div>
       {!start && <button onClick={handleStartGame}>Start the Game</button>}
       {start && (
         <div>
-          <h1>{timer}</h1>
+          <h1 className={hideTimer ? 'hidden' : ''}>{timer}</h1>
           <h1>{questions[index].question}</h1>
           {questions[index].options.map((option, index) => (
             <section key={index}>
