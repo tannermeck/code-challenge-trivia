016530fab6def300b71dcdb5fd4a6e4fd60ae76e
TANNER MECK <steve.meck246@gmail.com>
2022-04-13 18:04:02 -0700

    score and try again button added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   const [timer, setTimer] = useState(30);
   const [answerIndex, setAnswerIndex] = useState(0);
   const [disabled, setDisabled] = useState(false);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
     const fetchData = () => {
@@ -32,15 +33,13 @@ function App() {
   // };
   const handleStartGame = () => {
     setStart(true);
-    // setAnswerIndex(+questions[index].solutionIndex);
   };
   const handleSubmitAnswer = () => {
     setDisabled(true);
     if (+questions[index].solutionIndex === +answer) {
-      //turn the selected answer green
       setCorrect(true);
+      setScore(score + 1);
     } else {
-      //turn the correct answer red;
       setIncorrect(true);
     }
   };
@@ -64,6 +63,16 @@ function App() {
       setIncorrect(false);
     }
   };
+  const handleTryAgain = () => {
+    setScore(0);
+    setIndex(0);
+    setAnswerIndex(0);
+    setCorrect(null);
+    setIncorrect(null);
+    setDisabled(false);
+    console.log('index', index);
+    console.log('solution', questions[index].solutionIndex);
+  };
   return (
     <div>
       {!start && <button onClick={handleStartGame}>Start the Game</button>}
@@ -85,10 +94,16 @@ function App() {
           <button disabled={disabled} onClick={handleSubmitAnswer}>
             Submit Answer!
           </button>
-          {/* {correct && <h1>Correct!!!</h1>}
-          {incorrect && <h1>Incorrect..</h1>} */}
           {correct && index < 2 && <button onClick={handleNext}>Next</button>}
           {incorrect && index < 2 && <button onClick={handleNext}>Next</button>}
+          {(correct || incorrect) && index === 2 && (
+            <button onClick={() => handleTryAgain()}>Try again?</button>
+          )}
+          {(correct || incorrect) && (
+            <h1>
+              Score: {score} / {index + 1}
+            </h1>
+          )}
         </div>
       )}
     </div>
